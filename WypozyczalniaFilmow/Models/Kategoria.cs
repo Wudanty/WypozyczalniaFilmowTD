7307334473894b79141b110e9db3308c4d17bacc
Wudanty <youheixd@gmail.com>
2022-02-28 14:01:15 +0100

    Add film and kategoria models, Add first db migration

---
+++ WypozyczalniaFilmow/Models/Kategoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WypozyczalniaFilmow.Models {
+    public class Kategoria {
+        public int Id { get; set; }
+        public string Nazwa { get;set; }
+        public string Opis { get;set; }
+        public ICollection<Film> Filmy { get; set; }
+
+       
+    }
+
+
+
+}
